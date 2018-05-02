14949d9d64d7a11ebc8de4187693ddbba4aa85de
Anna Townsend <31076277+annamtown@users.noreply.github.com>
2018-05-02 10:31:47 -0400

    add bowtie2 index command

--- rna_seq.sh
+++ rna_seq.sh
@@ -6,12 +6,18 @@
 ## Anna Townsend
 ## May 1 2018
 
-# reference genome fasta file was downloaded from ENA and copied using scp into directory 
+# reference genome fasta file was downloaded from ENA and copied using scp onto server to run this script
+# name of reference genome: AL591824.fasta
 
 # Download sequence reads
-fastq-dump --fasta SRR6281667    #EGD-e with D-glucose
+fastq-dump SRR6281667    #EGD-e with D-glucose
 
-fastq-dump --fasta SRR6281666    #EGD-e with D-allose
+fastq-dump SRR6281666    #EGD-e with D-allose
+
+# Index reference genome
+# -f indicates fasta format
+# -c indicates reference sequence given on cmd line (as <seq_in>)
+bowtie2-build -f AL591824.fasta ref_seq
 
 # run Bowtie2
 # bowtie2 [options]* -x <bt2-idx> {-1 <m1> -2 <m2> | -U <r>} [-S <sam>]
@@ -26,3 +32,4 @@ fastq-dump --fasta SRR6281666    #EGD-e with D-allose
 #
 # <m1>, <m2>, <r> can be comma-separated lists (no whitespace) and can be
 # specified many times  E.g. '-U file1.fq,file2.fq -U file3.fq'
+bowtie2 -x ref_seq -U SRR6281667.fasta -S SRR6281667.sam
